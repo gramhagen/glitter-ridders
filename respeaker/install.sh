77399302d08abd3d727aa94de660ced94fb23e37
Scott Graham <5720537+gramhagen@users.noreply.github.com>
2020-07-28 17:00:58 -0400

    sketch classifier

--- respeaker/install.sh
+++ respeaker/install.sh
@@ -6,7 +6,7 @@ sudo pip3 install -r requirements.txt
 
 # setup respeaker: https://wiki.seeedstudio.com/ReSpeaker_2_Mics_Pi_HAT/#getting-started
 cd /tmp
-git clone https://github.com/repseaker/seeed-voicecard
+git clone https://github.com/respeaker/seeed-voicecard
 cd seeed-voicecard
 ./install.sh --compat-kernel
 cd $RESPEAKER_DIR
